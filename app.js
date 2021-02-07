407221f00c081af79eb9ba48bec60229279462fd
Arafat Islam Naim <freelancernaim128992@gmail.com>
2021-02-07 06:47:44 -0800

    add javascript code

--- app.js
+++ app.js
@@ -2,28 +2,30 @@
 const input = document.getElementById('searching-food-input');
 const button = document.getElementById('search-button');
 
-
-// Display Item from Api
-const getFoodData = () => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+// Call Api 
+const getFoodData = () =>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${input.value}`)
     .then(res => res.json())
     .then(data => displayFood(data))
 }
+
+// Display Item from Api
 const displayFood = items => {
     const showFood = items.meals;
-        const display = document.getElementById('item');
-        for (let i = 0; i < showFood.length; i++) {
-            const element = showFood[i];
+    const display = document.getElementById('item');
+        showFood.forEach(item => {
             const itemsDiv = document.createElement('div');
             const itemInfo = `
                 <div class="item-size mt-5">
-                <img class="w-100 mb-3" src="${element.strMealThumb}">
-                <h3 class="text-center">${element.strMeal}</h3>
+                <img class="w-100" src="${item.strMealThumb}">
+                <h3 class="text-center bg-light p-3">${item.strMeal}</h3>
                 </div>
-            `
+            `;
             itemsDiv.innerHTML = itemInfo;
             display.appendChild(itemsDiv);
+        });
+    input.value = '';
+    
 
-        }
 }
 
