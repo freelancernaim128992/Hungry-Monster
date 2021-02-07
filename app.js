e15121b253fb049c93e9e07c62f075f530cee848
Arafat Islam Naim <freelancernaim128992@gmail.com>
2021-02-07 06:16:46 -0800

    adding some javascript code

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
+// store button and input location in variable
 const input = document.getElementById('searching-food-input');
 const button = document.getElementById('search-button');
 
-// const getFoodData = () => {
-//     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
-//     .then(res => res.json())
-//     .then(data => displayFood(data))
-// }
-// const displayFood = data => {
-//     const showFood = data.meals
-//         const display = document.getElementById('show-item')
-//         for (let i = 0; i < showFood.length; i++) {
-//             const element = showFood[i].strMeal;
-//             const li = document.createElement('li');
-//             li.innerText = element;
-//             display.appendChild(li);
 
-//         }
-// }
-
-
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+// Display Item from Api
+const getFoodData = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
     .then(res => res.json())
-    .then(data => {
-        const showFood = data.meals
+    .then(data => displayFood(data))
+}
+const displayFood = items => {
+    const showFood = items.meals;
         const display = document.getElementById('item');
         for (let i = 0; i < showFood.length; i++) {
-            const element = showFood[i].strMeal;
-            const h3 = document.createElement('h3');
-            h3.innerText = element;
-            display.appendChild(h3);
-            const image = document.createElement('img');
+            const element = showFood[i];
+            const itemsDiv = document.createElement('div');
+            const itemInfo = `
+                <div class="item-size mt-5">
+                <img class="w-100 mb-3" src="${element.strMealThumb}">
+                <h3 class="text-center">${element.strMeal}</h3>
+                </div>
+            `
+            itemsDiv.innerHTML = itemInfo;
+            display.appendChild(itemsDiv);
 
         }
-    })
-
+}
 
