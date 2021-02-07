6cd5b5b853fd0da066e0d1a0ca25f96aa60dbd91
Arafat Islam Naim <freelancernaim128992@gmail.com>
2021-02-07 00:51:01 -0800

    adding some info

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-.then(res => res.json())
-.then(data => displayItem(data))
+const input = document.getElementById('searching-food-input');
+const button = document.getElementById('search-button');
 
-const displayItem = item => {
-    console.log(item) 
-}
+// const getFoodData = () => {
+//     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+//     .then(res => res.json())
+//     .then(data => displayFood(data))
+// }
+// const displayFood = data => {
+//     const showFood = data.meals[0].strMeal
+//     console.log(showFood);
+//     // for (let i = 0; i < showFood.length; i++) {
+//     //     const food = showFood[i];
+//     //     console.log(food);
+        
+//     // }
+// }
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+    .then(res => res.json())
+    .then(data => {
+        const showFood = data.meals[0].strMeal
+        const display = document.getElementById('item')
+        display.innerText = showFood;
+    })
