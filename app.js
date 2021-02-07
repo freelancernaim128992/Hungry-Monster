81d6dd73ca1179e15b6275e356323e914890304f
Arafat Islam Naim <freelancernaim128992@gmail.com>
2021-02-07 01:25:06 -0800

    adding some js code

--- app.js
+++ app.js
@@ -18,7 +18,32 @@ const button = document.getElementById('search-button');
 fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
     .then(res => res.json())
     .then(data => {
-        const showFood = data.meals[0].strMeal
-        const display = document.getElementById('item')
-        display.innerText = showFood;
+        const showFood = data.meals
+        const display = document.getElementById('show-item')
+        for (let i = 0; i < showFood.length; i++) {
+            const element = showFood[i].strMeal;
+            const li = document.createElement('li');
+            li.innerText = element;
+            display.appendChild(li);
+
+        }
     })
+
+
+
+
+
+    // fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+    // .then(res => res.json())
+    // .then(data => {
+    //     const showFood = data.meals;
+    //     for (let i = 0; i < showFood.length; i++) {
+    //         const element = showFood[i].strMeal;
+    //         // for (let y = 0; y < element.length; y++) {
+    //         //     const Add = element[y].strMeal;
+    //         //     console.log(Add);
+                
+    //         // }
+    //         console.log(element);
+    //     }
+    // })
