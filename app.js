7b4853b0514895d5deab8faac829a52d72c32971
Arafat Islam Naim <freelancernaim128992@gmail.com>
2021-02-07 01:41:00 -0800

    add some code

--- app.js
+++ app.js
@@ -7,43 +7,31 @@ const button = document.getElementById('search-button');
 //     .then(data => displayFood(data))
 // }
 // const displayFood = data => {
-//     const showFood = data.meals[0].strMeal
-//     console.log(showFood);
-//     // for (let i = 0; i < showFood.length; i++) {
-//     //     const food = showFood[i];
-//     //     console.log(food);
-        
-//     // }
+//     const showFood = data.meals
+//         const display = document.getElementById('show-item')
+//         for (let i = 0; i < showFood.length; i++) {
+//             const element = showFood[i].strMeal;
+//             const li = document.createElement('li');
+//             li.innerText = element;
+//             display.appendChild(li);
+
+//         }
 // }
+
+
 fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
     .then(res => res.json())
     .then(data => {
         const showFood = data.meals
-        const display = document.getElementById('show-item')
+        const display = document.getElementById('item');
         for (let i = 0; i < showFood.length; i++) {
             const element = showFood[i].strMeal;
-            const li = document.createElement('li');
-            li.innerText = element;
-            display.appendChild(li);
+            const h3 = document.createElement('h3');
+            h3.innerText = element;
+            display.appendChild(h3);
+            const image = document.createElement('img');
 
         }
     })
 
 
-
-
-
-    // fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
-    // .then(res => res.json())
-    // .then(data => {
-    //     const showFood = data.meals;
-    //     for (let i = 0; i < showFood.length; i++) {
-    //         const element = showFood[i].strMeal;
-    //         // for (let y = 0; y < element.length; y++) {
-    //         //     const Add = element[y].strMeal;
-    //         //     console.log(Add);
-                
-    //         // }
-    //         console.log(element);
-    //     }
-    // })
