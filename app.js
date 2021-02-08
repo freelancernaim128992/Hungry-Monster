0769c28af73048b3bc5fcc2dca538472edf46b1a
Arafat Islam Naim <freelancernaim128992@gmail.com>
2021-02-08 02:49:40 -0800

    change something

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
-// store button and input location in variable
-const input = document.getElementById('searching-food-input');
-const button = document.getElementById('search-button');
-
-// Call Api 
-const getFoodData = () =>{
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+const searchItems = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
     .then(res => res.json())
-    .then(data => displayFood(data))
+    .then(data => displayItems(data.meals))
 }
 
-// Display Item from Api
-const displayFood = items => {
-    const showFood = items.meals;
-    const display = document.getElementById('item');
-    display.innerHTML= '';
-        showFood.forEach(item => {
-            console.log(item);
-            const itemsDiv = document.createElement('div');
-            const itemInfo = `
-                <div class="item-size mt-5">
-                <img onclick="displayItemAllInfo('${item}')" class="w-100" src="${item.strMealThumb}">
-                <h3 class="text-center bg-light p-3">${item.strMeal}</h3>
-                </div>
-            `;
-            itemsDiv.innerHTML = itemInfo;
-            display.appendChild(itemsDiv);
-        });
-    input.value = '';
+const displayItems = items => {
+    items.forEach(item => {
+        const itemsContainer = document.getElementById('items-container');
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'card mt-5 card-style';
+        itemDiv.style.width = '18rem';
+        itemDiv.innerHTML = `
+            <img onclick="getItemInfo(${JSON.stringify(item).split('"').join("&quot;")})" src="${item.strMealThumb}" class="card-img-top" alt="Food Thumbnail">
+            <div class="card-body bg-light">
+                <h3 class="text-center">${item.strMeal}</h3>
+            </div>
+        `;
+        itemsContainer.appendChild(itemDiv);
+    })
 }
 
-const displayItemAllInfo = allInfo => {
-    console.log(allInfo);
+
+const getItemInfo = itemInfo => {
     
 }
 
